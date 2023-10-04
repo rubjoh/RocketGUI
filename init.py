b1272e4ce107fdd6bc174ce15fe152afd1071f07
RubenAHJ <rub1hanss1@gmail.com>
2023-10-04 21:41:25 +0200

    Added info on how to install

--- init.py
+++ init.py
@@ -1,1 +1,8 @@
-# Fist commit 
+# Install pyqt6 on your system:
+# 1. Open terminal 
+# 2. Use command: pip3 install pyqt6
+# NB: make sure python and pip is installed on your system 
+
+# Initialize QT
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
